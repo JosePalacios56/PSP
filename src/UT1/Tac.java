11890a9e13c9d1d09b5dde355f3428d3e70e3d40
JosePalacios56 <Josepalaciosies@gmail.com>
2019-10-01 11:56:22 +0200

    tac

---
+++ src/UT1/Tac.java
@@ -0,0 +1,16 @@
+package UT1.EjercicioTic_Tac;
+
+public class Tac extends Thread{
+
+    @Override
+    public void run() {
+        super.run();
+        System.out.println("TAC");
+        try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
