52e22003da71655375318a228eecfed0ce8458c5
JosePalacios56 <Josepalaciosies@gmail.com>
2019-10-08 12:11:47 +0200

    actualizado2

--- src/UT2/Cajera.java
+++ src/UT2/Cajera.java
@@ -1,13 +1,16 @@
 package src.UT2;
 
 public class Cajera {
+
     private String nombre;
 
 
     public Cajera(String nombre) {
+        
         this.nombre = nombre;
     }
     public void procesarCompra(Cliente cliente, long timeStamp){
+
         System.out.println("La cajera "+this.nombre+
                 "COMIENZA A PROCESAR LA COMPRA DEL CLIENTE"+cliente.getNombre()+
                 "EN EL TIEMPO"+ (System.currentTimeMillis()-timeStamp)/1000+" seg");
---
+++ src/UT2/Main.java
@@ -0,0 +1,12 @@
+package src.UT2;
+
+public class Main {
+    public static void main(String[]args){
+
+        Cliente cliente1 = new Cliente("Cliente1",new int[]{2,2,1,5,2,3});
+        Cliente cliente2 = new Cliente("Cliente2",new int[]{1,3,5,1,1});
+
+        Cajera cajera1=new Cajera("Cajera1");
+        Cajera cajera2=new Cajera("Cajera2");
+    }
+}
