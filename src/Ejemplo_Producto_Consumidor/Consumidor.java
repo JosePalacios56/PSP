6c490019ec0af6a12d1584a69496253c9d56492e
JosePalacios56 <Josepalaciosies@gmail.com>
2019-10-15 12:31:32 +0200

    actualizado2

---
+++ src/Ejemplo_Producto_Consumidor/Consumidor.java
@@ -0,0 +1,19 @@
+package src.Ejemplo_Producto_Consumidor;
+
+public class Consumidor extends Thread{
+    private Cola cola;
+    private int n;
+
+    public Consumidor(Cola c ,int n){
+        this.cola=c;
+        this.n=n;
+    }
+
+    public void run(){
+        int valor=0;
+        for(int i=0;i<5;i++){
+            valor= cola.get();
+            System.out.println(i+" =>Consumidor : "+n+" consume :"+ valor);
+        }
+    }
+}
